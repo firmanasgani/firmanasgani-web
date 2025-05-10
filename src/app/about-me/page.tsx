28cfae037a526fdd2e7604a0ae66d38535e9af16
Firman Maulana <firmanasgani37@gmail.com>
2025-05-10 16:11:04 +0700

    add target blank

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -39,14 +39,21 @@ export default function AboutPage() {
           </li>
           <li>
             <strong>Github:</strong>{" "}
-            <Link href="https://github.com/firmanasgani" className="text-blue-600"
-            target="_blank">
+            <Link
+              href="https://github.com/firmanasgani"
+              className="text-blue-600"
+              target="_blank"
+            >
               https://github.com/firmanasgani
             </Link>
           </li>
-           <li>
+          <li>
             <strong>LinkedIn:</strong>{" "}
-            <Link href="https://linkedin.com/in/firmanasgani" className="text-blue-600"    target="_blank">
+            <Link
+              href="https://linkedin.com/in/firmanasgani"
+              className="text-blue-600"
+              target="_blank"
+            >
               https://linkedin.com/in/firmanasgani
             </Link>
           </li>
@@ -73,13 +80,13 @@ export default function AboutPage() {
               (2024–Sekarang)
             </li>
             <li>
-              <strong>Web Developer And Analyst</strong> – PT. Bayu Buana Gemilang
-              (2020–2024)
+              <strong>Web Developer And Analyst</strong> – PT. Bayu Buana
+              Gemilang (2020–2024)
             </li>
           </ul>
         </div>
 
-          <div>
+        <div>
           <h3 className="text-xl font-semibold mb-4">Tech Stack</h3>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 text-gray-700">
@@ -125,36 +132,35 @@ export default function AboutPage() {
         </div>
       </div>
       <div className="mt-12">
-  <h2 className="text-2xl font-semibold mb-4">GitHub Contribution</h2>
+        <h2 className="text-2xl font-semibold mb-4">GitHub Contribution</h2>
 
-  {/* Contribution Chart */}
-  <div className="bg-white shadow rounded-xl p-4 mb-6">
-    <h3 className="text-lg font-medium mb-2">Contribution Graph</h3>
-    <iframe
-      src="https://ghchart.rshah.org/firmanasgani"
-      title="GitHub Contributions"
-      width="100%"
-      height="150"
-      className="w-full max-w-xl"
-    />
-    <p className="text-sm text-gray-500 mt-2">
-      Grafik kontribusi tahunan dari GitHub
-    </p>
-  </div>
+        {/* Contribution Chart */}
+        <div className="bg-white shadow rounded-xl p-4 mb-6">
+          <h3 className="text-lg font-medium mb-2">Contribution Graph</h3>
+          <iframe
+            src="https://ghchart.rshah.org/firmanasgani"
+            title="GitHub Contributions"
+            width="100%"
+            height="150"
+            className="w-full max-w-xl"
+          />
+          <p className="text-sm text-gray-500 mt-2">
+            Grafik kontribusi tahunan dari GitHub
+          </p>
+        </div>
 
-  {/* GitHub Stats */}
-  <div className="bg-white shadow rounded-xl p-4">  
-    <img
-      src="https://github-readme-stats.vercel.app/api?username=firmanasgani&show_icons=true&theme=default"
-      alt="GitHub Stats"
-      className="w-full max-w-xl"
-    />
-    <p className="text-sm text-gray-500 mt-2">
-      Statistik umum dari GitHub: repo, commit, followers
-    </p>
-  </div>
-</div>
-
+        {/* GitHub Stats */}
+        <div className="bg-white shadow rounded-xl p-4">
+          <img
+            src="https://github-readme-stats.vercel.app/api?username=firmanasgani&show_icons=true&theme=default"
+            alt="GitHub Stats"
+            className="w-full max-w-xl"
+          />
+          <p className="text-sm text-gray-500 mt-2">
+            Statistik umum dari GitHub: repo, commit, followers
+          </p>
+        </div>
+      </div>
 
       {/* Download CV */}
       {/* <div className="mt-10">
